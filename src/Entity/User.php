60c924942f084c619531646b430a8dc75e3e2098
laaouafi ishak <laaouafi6@gmail.com>
2025-12-30 16:09:18 +0100

    class des roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,35 +1,59 @@
 <?php
 include_once "config\database.php";
-class User {
+include_once "src\Entity\role.php";
+class User
+{
     protected $id;
     protected $username;
     protected $password;
     protected $date_inscription;
-    protected $role;
-    public function __construct($id,$username,$password,$role,$date_inscription=null)
-    {
-        $this->id=$id;
-        $this->username=$username;
-        $this->password=$password;
-        $this->date_inscription=$date_inscription;
-        $this->role= $role;
-        
+    protected role $role;
 
+    public function __construct($id, $username, $password, $date_inscription = null)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->date_inscription = $date_inscription;
     }
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
-    public function getUsername(){
+    public function getUsername()
+    {
         return $this->username;
     }
-    public function getPassword(){
+    public function getPassword()
+    {
         return $this->password;
     }
-    public function getDate_inscription(){
+    public function getDate_inscription()
+    {
         return $this->date_inscription;
     }
-    public function getRole_name(){
-        // return $this->role_name;
+    public function getRole()
+    {
+        return $this->role;
     }
-    
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function setDate_inscription($date_inscription)
+    {
+        $this->date_inscription = $date_inscription;
+    }
 }
---
+++ src/Entity/role.php
@@ -0,0 +1,27 @@
+<?php
+include_once "config\database.php";
+include_once "src\Entity\User.php";
+class Role{
+    private $id;
+    private $name;
+    
+    public function __construct($id,$name)
+    {
+        $this->id=$id;
+        $this->name=$name;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setId($id){
+        $this->id=$id;
+    }
+    public function setName($name){
+        $this->name=$name;
+    }
+
+    
+}
