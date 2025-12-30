5d44a18c5450e969a324444a90a3a2d13a3ef03b
elharchaouyilham <elharchaouyilham77@gmail.com>
2025-12-30 16:16:08 +0100

    class theme

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -1,4 +1,32 @@
 <?php
-class Theme{
+class Theme
+{
 
+    private $id;
+
+    private $nom;
+
+    private $couleur;
+
+    public function __construct($id=null ,$nom, $couleur)
+    {
+        $this->nom = $nom;
+        $this->couleur = $couleur;
+    }
+    public function __setName($nom, $value)
+    {
+        $this->$nom = $value;
+    }
+    public function __setColor($couleur, $value)
+    {
+        $this->$couleur = $value;
+    }
+    public function __getName($nom)
+    {
+        return $this->$nom;
+    }
+    public function __getColor($couleur)
+    {
+        return $this->$couleur;
+    }
 }
