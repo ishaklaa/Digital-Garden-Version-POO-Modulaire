29dbbbdfe078c46e1be64a5230b03427b2b12e1d
elharchaouyilham <elharchaouyilham77@gmail.com>
2025-12-30 10:13:38 +0100

    class database

--- config/database.php
+++ config/database.php
@@ -1,3 +1,25 @@
-class database{
-    
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "dgarden";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Erreur connexion: " . $e->getMessage();
+        }
+    }
+    public function getConnection() {
+        return $this->conn;
+    }
 }
