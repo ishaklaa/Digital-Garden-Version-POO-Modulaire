f897e119c5f0a66526b173f91b36aa07c1bb262a
elharchaouyilham <elharchaouyilham77@gmail.com>
2026-01-03 18:19:02 +0100

    validation js

--- public/register.php
+++ public/register.php
@@ -10,7 +10,7 @@
   <div class="card p-4 mx-auto form-box">
     <h3 class="text-center">Créer un compte</h3>
 
-    <form action ="" method="post">
+    <form action ="" method="post" id="registerForm">
       <input type="text" class="form-control mb-3"  name="nom" placeholder="Nom d'utilisateur" required>
       <input type="email" class="form-control mb-3"name="email" placeholder="Email" required>
       <input type="password" class="form-control mb-3" name="password" placeholder="Mot de passe" required>
