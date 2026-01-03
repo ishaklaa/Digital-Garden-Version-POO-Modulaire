aebfb9d308e9697d58b2cdf09aea5496a3e74d2d
elharchaouyilham <elharchaouyilham77@gmail.com>
2026-01-03 19:48:36 +0100

    fixer fichier

--- public/login.php
+++ public/login.php
@@ -1,6 +1,4 @@
-<?php include '../includes/header.php';
-
- ?>
+<?php include '../includes/header.php'; ?>
 <?php
 session_start()
 
@@ -10,7 +8,7 @@ session_start()
   <div class="card p-4 mx-auto form-box">
     <h3 class="text-center">Connexion </h3>
 
-    <form action ="../src/Entity/Service/AuthService.php" method="post" >
+    <form action ="../src/Service/AuthService.php" method="post" >
       <input type="email" class="form-control mb-3" name="email" placeholder="Email" required>
       <input type="password" class="form-control mb-3" name="password" placeholder="Mot de passe" required>
       <button class="btn btn-success w-100" name="login">Connexion</button>
--- public/register.php
+++ public/register.php
@@ -1,18 +1,18 @@
 <?php
- include '../includes/header.php';
- require "../src/Repository/UserRepository.php";
- ?>
- 
- 
+include '../includes/header.php';
+require "../src/Repository/UserRepository.php";
+?>
+
+
 <?php
 ?>
 <div class="container mt-5">
   <div class="card p-4 mx-auto form-box">
     <h3 class="text-center">Créer un compte</h3>
 
-    <form action ="" method="post" id="registerForm">
-      <input type="text" class="form-control mb-3"  name="nom" placeholder="Nom d'utilisateur" required>
-      <input type="email" class="form-control mb-3"name="email" placeholder="Email" required>
+    <form action="" method="post" id="registerForm">
+      <input type="text" class="form-control mb-3" name="nom" placeholder="Nom d'utilisateur" required>
+      <input type="email" class="form-control mb-3" name="email" placeholder="Email" required>
       <input type="password" class="form-control mb-3" name="password" placeholder="Mot de passe" required>
       <button class="btn btn-success w-100" name="inscription">Inscription</button>
     </form>
