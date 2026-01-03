4924fe33b0e8367516aa94e3f4bf20cefd6e5f92
elharchaouyilham <elharchaouyilham77@gmail.com>
2026-01-03 17:54:12 +0100

    modifier fichier register

--- public/register.php
+++ public/register.php
@@ -10,11 +10,11 @@
   <div class="card p-4 mx-auto form-box">
     <h3 class="text-center">Créer un compte</h3>
 
-    <form method="post">
-      <input type="text" class="form-control mb-3" placeholder="Nom d'utilisateur" required>
-      <input type="email" class="form-control mb-3" placeholder="Email" required>
-      <input type="password" class="form-control mb-3" placeholder="Mot de passe" required>
-      <button class="btn btn-success w-100">Inscription</button>
+    <form action ="" method="post">
+      <input type="text" class="form-control mb-3"  name="nom" placeholder="Nom d'utilisateur" required>
+      <input type="email" class="form-control mb-3"name="email" placeholder="Email" required>
+      <input type="password" class="form-control mb-3" name="password" placeholder="Mot de passe" required>
+      <button class="btn btn-success w-100" name="inscription">Inscription</button>
     </form>
   </div>
 </div>
